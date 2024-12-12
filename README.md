5efa44a9cc06442b62823c34fb1b88e3f41c5398
rezaulkarim20 <rezaulkarim958672@gmail.com>
2024-12-12 19:28:45 +0600

    readme file update

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Project name: sunnah-store
-
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,7 @@
-
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="text-3xl text-center bg-green-500 px-3 py-2">Home</div>
+  );
+};
 
-export default Home
+export default Home;
